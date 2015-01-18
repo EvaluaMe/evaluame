bd0d66478c9ba4fdbaff2b6d0ed6a1490b7790dc
Sergio García <garciparedes@gmail.com>
2015-01-18 20:52:33 +0100

    Improved Add Test Fragment

--- app/src/main/java/com/garciparedes/resultator/FragmentAddTest.java
+++ app/src/main/java/com/garciparedes/resultator/FragmentAddTest.java
@@ -15,12 +15,9 @@ import android.widget.Toast;
 import com.doomonafireball.betterpickers.numberpicker.NumberPickerBuilder;
 import com.doomonafireball.betterpickers.numberpicker.NumberPickerDialogFragment;
 
-/**
- * Created by garciparedes on 10/12/14.
- */
+
 public class FragmentAddTest extends Fragment
         implements NumberPickerDialogFragment.NumberPickerDialogHandler{
-
 
     private int subject;
     private Button btnCreate;
@@ -30,9 +27,12 @@ public class FragmentAddTest extends Fragment
     private TextView textMark;
     private TextView textValue;
 
+    private NumberPickerBuilder numberPickerMark;
+    private NumberPickerBuilder numberPickerValue;
+
     private String name;
     private float score;
-    private int value;
+    private float value;
 
 
     public static FragmentAddTest newInstance(int i) {
@@ -52,8 +52,8 @@ public class FragmentAddTest extends Fragment
         subject = getArguments().getInt("subject", 0);
 
         // Set the dialog text -- this is better done in the XML
-        textMark = (TextView) view.findViewById(R.id.text_view_dialog_mark);
-        textValue = (TextView) view.findViewById(R.id.text_view_dialog_value);
+        textMark = (TextView) view.findViewById(R.id.textView_set_mark);
+        textValue = (TextView) view.findViewById(R.id.textView_set_value);
         editTextName = (EditText) view.findViewById(R.id.edit_text_dialog);
         btnCreate = (Button) view.findViewById(R.id.button_dialog);
 
@@ -64,44 +64,38 @@ public class FragmentAddTest extends Fragment
     public void onActivityCreated(Bundle state) {
         super.onActivityCreated(state);
 
-
         editTextName.setHint(getString(R.string.set_name));
-        textMark.setText(getString(R.string.set_score));
-        textValue.setText(getString(R.string.set_value));
+
+        numberPickerMark = new NumberPickerBuilder()
+                .setFragmentManager(getFragmentManager())
+                .setStyleResId(R.style.BetterPickersDialogFragment)
+                .setTargetFragment(FragmentAddTest.this)
+                .setMaxNumber(10)
+                .setMinNumber(0)
+                .setPlusMinusVisibility(View.INVISIBLE)
+                .setReference(0);
+
+        numberPickerValue = new NumberPickerBuilder()
+                .setFragmentManager(getFragmentManager())
+                .setStyleResId(R.style.BetterPickersDialogFragment)
+                .setTargetFragment(FragmentAddTest.this)
+                .setMaxNumber(100)
+                .setMinNumber(0)
+                .setPlusMinusVisibility(View.INVISIBLE)
+                .setLabelText("%")
+                .setReference(1);
 
         textMark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), getString(R.string.error_max_score), Toast.LENGTH_SHORT).show();
-
-                //DecimalNumberDialog subjectDialog = new DecimalNumberDialog(getActivity());
-                //subjectDialog.show();
-                NumberPickerBuilder numberPickerBuilder = new NumberPickerBuilder()
-                        .setFragmentManager(getFragmentManager())
-                        .setStyleResId(R.style.BetterPickersDialogFragment)
-                        .setTargetFragment(FragmentAddTest.this)
-                        .setReference(0);
-
-                numberPickerBuilder.show();
-
+                numberPickerMark.show();
             }
         });
 
         textValue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), getString(R.string.error_max_score), Toast.LENGTH_SHORT).show();
-
-                //DecimalNumberDialog subjectDialog = new DecimalNumberDialog(getActivity());
-                //subjectDialog.show();
-                NumberPickerBuilder numberPickerBuilder = new NumberPickerBuilder()
-                        .setFragmentManager(getFragmentManager())
-                        .setStyleResId(R.style.BetterPickersDialogFragment)
-                        .setTargetFragment(FragmentAddTest.this)
-                        .setReference(1);
-
-                numberPickerBuilder.show();
-
+                numberPickerValue.show();
             }
         });
 
@@ -115,6 +109,7 @@ public class FragmentAddTest extends Fragment
 
                 if (score > 10) {
                     Toast.makeText(getActivity(), getString(R.string.error_max_score), Toast.LENGTH_SHORT).show();
+
                 } else {
 
                     ListDB.addTest(subject, name, score, value);
@@ -137,12 +132,12 @@ public class FragmentAddTest extends Fragment
     public void onDialogNumberSet(int reference, int number, double decimal, boolean isNegative, double fullNumber) {
         switch (reference){
             case 0:
-                textMark.setText("Puntuacion " + fullNumber);
+                textMark.setText(fullNumber+"");
                 score = (float) fullNumber;
                 break;
             case 1:
-                textValue.setText("Valor " +number);
-                value = number;
+                textValue.setText(fullNumber + "%");
+                value = (float) fullNumber;
                 break;
 
             default:
