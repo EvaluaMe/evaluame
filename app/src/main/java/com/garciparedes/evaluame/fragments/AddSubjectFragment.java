1a2d63a7bef4e002f9c20152c746c05f38913d14
Sergio García <garciparedes@gmail.com>
2015-02-10 22:47:39 +0100

    Error committing

---
+++ app/src/main/java/com/garciparedes/evaluame/fragments/AddSubjectFragment.java
@@ -0,0 +1,50 @@
+package com.garciparedes.evaluame.fragments;
+
+
+import android.os.Bundle;
+
+import com.garciparedes.evaluame.R;
+import com.garciparedes.evaluame.items.Subject;
+import com.garciparedes.evaluame.provider.ListDB;
+
+/**
+ * FramentAddSubject Class
+ */
+public class AddSubjectFragment extends BaseManageSubjectFragment{
+    /**
+     * NewInstance method
+     *
+     * @return AddSubjectFragment
+     */
+    public static AddSubjectFragment newInstance() {
+        AddSubjectFragment f = new AddSubjectFragment();
+        Bundle args = new Bundle();
+        f.setArguments(args);
+        return f;
+    }
+
+    @Override
+    public void setOnClickButton() {
+        ListDB.addSubject(getActivity(), newSubject);
+    }
+
+    @Override
+    public String setTextButton() {
+        return getString(R.string.create_subject);
+    }
+
+    @Override
+    public Subject initSubject() {
+        return new Subject();
+    }
+
+    @Override
+    public String setTextName() {
+        return null;
+    }
+
+    @Override
+    public String setTextDescription() {
+        return null;
+    }
+}
