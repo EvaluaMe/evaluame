a22a6a83080a406c155f21464584be6974156311
Sergio García <garciparedes@gmail.com>
2015-07-16 14:27:02 +0200

    Creating login activity

--- app/src/main/java/com/garciparedes/evaluame/application/EvaluaMe.java
+++ app/src/main/java/com/garciparedes/evaluame/application/EvaluaMe.java
@@ -3,7 +3,6 @@ package com.garciparedes.evaluame.application;
 import android.app.Application;
 import android.content.res.Configuration;
 
-import com.parse.Parse;
 
 /**
  * Created by garciparedes on 15/7/15.
