ee9fd8a81a99e22aa3764c2547ff787641faca17
Sergio García <garciparedes@gmail.com>
2015-07-21 15:39:38 +0200

    Added Parse

--- app/src/main/java/com/garciparedes/evaluame/application/EvaluaMe.java
+++ app/src/main/java/com/garciparedes/evaluame/application/EvaluaMe.java
@@ -4,12 +4,15 @@ import android.app.Application;
 import android.content.res.Configuration;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 /**
  * Created by garciparedes on 15/7/15.
  */
 public class EvaluaMe extends Application {
 
+    private static final String APPLICATION_ID = "Irm18S3p9pCtusyGlCHE7212JPPCkPu9A8Ev9P5r";
+    private static final String CLIENT_KEY = "BJ5Gi7S83xurjLe3CS1U4ogW8BixKRb39TL2aqkL";
     private static EvaluaMe singleton;
 
     public static EvaluaMe getInstance() {
@@ -25,7 +28,15 @@ public class EvaluaMe extends Application {
     public void onCreate() {
         super.onCreate();
         singleton = this;
+
         // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
 
     }
 
