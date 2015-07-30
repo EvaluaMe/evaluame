2c8b64c949eaca292f65bd4570335f13eb078092
Sergio García <garciparedes@gmail.com>
2015-07-30 20:01:37 +0200

    LogOut implemented and partially implemented SetUsername

--- app/src/main/java/com/garciparedes/evaluame/activities/SettingsActivity.java
+++ app/src/main/java/com/garciparedes/evaluame/activities/SettingsActivity.java
@@ -1,10 +1,19 @@
 package com.garciparedes.evaluame.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.garciparedes.evaluame.R;
 import com.parse.ParseUser;
@@ -35,13 +44,73 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
 
-    public static class MyPreferenceFragment extends PreferenceFragment
-    {
+    public static class MyPreferenceFragment extends PreferenceFragment {
+
+        private ParseUser parseUser;
+
+        private SharedPreferences.Editor editor;
+        private SharedPreferences sharedPreferences;
+
         @Override
         public void onCreate(final Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
+
+            parseUser = ParseUser.getCurrentUser();
+
+            sharedPreferences =PreferenceManager.getDefaultSharedPreferences(getActivity());
+            editor = sharedPreferences.edit();
+
+            editor.putString("email", ParseUser.getCurrentUser().getEmail());
+            editor.putString("username", ParseUser.getCurrentUser().getUsername());
+
+
+            editor.apply();
+
             addPreferencesFromResource(R.xml.settings);
+
+            EditTextPreference editTextPreferenceUsername = (EditTextPreference) findPreference("username");
+
+            editTextPreferenceUsername.setSummary(sharedPreferences.getString("username", ""));
+        }
+
+        public SettingsActivity getSettingActivity(){
+            return  (SettingsActivity) getActivity();
+        }
+
+
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            switch (preference.getTitleRes()){
+                case (R.string.receive_notifications):
+                    Snackbar.make(getView(), "Hola", Snackbar.LENGTH_LONG).show();
+                    return true;
+
+                case (R.string.username):
+
+                    String string = sharedPreferences.getString("username", "");
+                    Snackbar.make(getView(), string, Snackbar.LENGTH_LONG).show();
+                    preference.setSummary(string);
+                    parseUser.setUsername(string);
+                    parseUser.saveInBackground();
+
+                    Toast.makeText(getActivity(),parseUser.getUsername(), Toast.LENGTH_LONG).show();
+
+                    return true;
+
+                case (R.string.email):
+
+                    return true;
+
+                case (R.string.logout):
+                    Snackbar.make(getView(), "Hola", Snackbar.LENGTH_LONG).show();
+                    getSettingActivity().logOut();
+                    return true;
+
+            }
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+
         }
     }
 }
