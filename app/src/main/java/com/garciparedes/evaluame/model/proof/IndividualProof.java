2b9dfbae99d71d663dba6f5454bf0811fa25e839
Sergio García <garciparedes@gmail.com>
2015-08-01 13:24:45 +0200

    Created Proof (and their subclasses), Score and Student classes and deleted Mark and Rational

---
+++ app/src/main/java/com/garciparedes/evaluame/model/proof/IndividualProof.java
@@ -0,0 +1,7 @@
+package com.garciparedes.evaluame.model.proof;
+
+/**
+ * Created by garciparedes on 1/8/15.
+ */
+public class IndividualProof {
+}
---
+++ app/src/main/java/com/garciparedes/evaluame/model/proof/Proof.java
@@ -0,0 +1,9 @@
+package com.garciparedes.evaluame.model.proof;
+
+/**
+ * Created by garciparedes on 1/8/15.
+ */
+public abstract class Proof {
+
+
+}
